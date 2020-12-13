ff53a600d0a81b1415c1ce34f78facf17228e304
TChristensenDrumz <tchristensendrumz@gmail.com>
2020-12-12 22:15:38 -0800

    Added functionality to all files

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,10 @@ DROP DATABASE IF EXISTS burgers_db;
 
 CREATE DATABASE burgers_db;
 USE burgers_db;
+
+CREATE TABLE burgers (
+id INT AUTO_INCREMENT NOT NULL,
+name VARCHAR(255) NOT NULL,
+eaten BOOLEAN DEFAULT false,
+PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Triple Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
